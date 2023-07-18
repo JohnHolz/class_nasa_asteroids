1463bb9dfe06c3a69e083416414c2cae3f3c72ea
johnholz <joaopaulo.paivaholz@gmail.com>
2023-07-17 23:02:35 -0300

    kfold pra rodar

--- modules/models.py
+++ modules/models.py
@@ -1,28 +1,32 @@
 import tensorflow as tf
+from tensorflow.keras.layers import Dense
+from tensorflow.keras import Sequential
 
 ## optimizer
 rms = "rmsprop"
 adam = "adam"
-
-## loss
 SPC = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 CC = tf.keras.losses.CategoricalCrossentropy()
 BC = "binary_crossentropy"
 MSE = "mse"
 SCC = "sparse_categorical_crossentropy"
-
-## last layer
 softmax = "softmax"
-sigmoid = "sigmoid"
 
 
-def model_3():
-    model = tf.keras.Sequential()
-    model.add(tf.keras.layers.Dense(8, activation="relu", input_shape=(43,)))
-    model.add(tf.keras.layers.Dense(16, activation="relu"))
-    model.add(tf.keras.layers.Dense(10, activation="relu"))
-    model.add(tf.keras.layers.Dense(1, activation="sigmoid"))  # sigmoid
-    model.compile(
-        loss=BC, optimizer=adam, metrics=[tf.keras.metrics.Precision(), "accuracy"]
+def multi_layer_model(
+    n_layers=[16, 8],
+    loss="binary_crossentropy",
+    optimizer="adam",
+    last_layer_activation="sigmoid",
+):
+    tf_model = Sequential()
+    tf_model.add(Dense(10, activation="relu", input_shape=(38,)))
+    for i in n_layers:
+        tf_model.add(Dense(i, activation="relu"))
+    tf_model.add(Dense(1, activation=last_layer_activation))
+    tf_model.compile(
+        loss=loss,
+        optimizer=optimizer,
+        metrics=[tf.keras.metrics.Precision(), "accuracy"],
     )
-    return model
+    return tf_model
